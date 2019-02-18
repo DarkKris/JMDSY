9e342214dbb8152e3718e100266d947baa9cd08e
DarkKris <long4664030@163.com>
2019-02-18 19:05:12 +0800

    fix: fix file

--- AMS1/main.c
+++ AMS1/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define _CRT_SECURE_NO_WARNINGS
+#define DEBUG
+
 int opt; // opt the user chosed in the menu
 
 void outputMenu() {
-	printf("欢迎进入计费管理系统\n----------菜单----------\n");
+	printf("----------菜单----------\n");
 	printf("1.添加卡\n2.查询卡\n3.上机\n4.下机\n5.充值\n6.退费\n7.查询统计\n8.注销卡\n0.退出");
 	printf("\n------------------------\n");
-	printf("请输入功能编号：");
+	printf("请输入功能编号：(0~8)");
 	scanf("%d",&opt);
 }
 
@@ -45,6 +48,7 @@ void exitCard() {
 
 void main() {
 	do {
+		printf("欢迎进入计费管理系统\n");
 		outputMenu();
 		switch(opt){
 		case 1:addCard();break;
@@ -55,6 +59,8 @@ void main() {
 		case 6:withdraw();break;
 		case 7:searchAll();break;
 		case 8:exitCard();break;
+		case 0:break;
+		default: printf("请正确输入编码!\n");
 		}
 	}while(opt != 0);
 	// return 0;
